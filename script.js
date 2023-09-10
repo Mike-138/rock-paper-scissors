2bc6d9327e1252f4627b48ca1a3b9ff1c54548e8
Michael <mikemcd.bbs@gmail.com>
2023-09-09 23:40:18 -0400

    Complete game logic to run 5 consecutive rounds
    Allows full games to be played instead of individual rounds

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function getPlayerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return `Draw! Both players chose ${playerSelection}!`;
+        return [`Draw! Both players chose ${playerSelection}!`, 0.5, 0.5];
     }
     let getIndex = function(choice) {
         for (let i = 0; i < OPTIONS.length; i++) {
@@ -26,7 +26,26 @@ function playRound(playerSelection, computerSelection) {
     const playerIndex = getIndex(playerSelection);
     const computerIndex = getIndex(computerSelection);
     if (playerIndex === computerIndex + 1 || playerIndex === computerIndex - 2) {
-        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        return [`You Win! ${playerSelection} beats ${computerSelection}!`, 1, 0];
     }
-    return `You Lose! ${computerSelection} beats ${playerSelection}!`
+    return [`You Lose! ${computerSelection} beats ${playerSelection}!`, 0, 1];
+}
+
+function game() {
+    let playerPointsTotal = 0;
+    let computerPointsTotal = 0;
+    for (let i = 0; i < 5; i++) {
+        let [roundResult, playerPointsEarned, computerPointsEarned] = playRound(getPlayerChoice(), getComputerChoice());
+        playerPointsTotal += playerPointsEarned;
+        computerPointsTotal += computerPointsEarned;
+        console.log(roundResult + `\nCurrent Score:\nPlayer -> ${playerPointsTotal}\nComputer -> ${computerPointsTotal}`);
+    }
+    console.log(`The final score is ${playerPointsTotal}-${computerPointsTotal}`);
+    if (playerPointsTotal > computerPointsTotal) {
+        console.log("You Win!");
+    } else if (playerPointsTotal < computerPointsTotal) {
+        console.log("You Lose!");
+    } else {
+        console.log("Draw!");
+    }
 }
